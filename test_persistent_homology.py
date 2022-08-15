e078542eea9d9235cc8ca853d9011db080dfefc5
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-08-15 17:18:14 -0500

    Update test_persistent_homology.py

--- test_persistent_homology.py
+++ test_persistent_homology.py
@@ -6,51 +6,76 @@ from typing import *
 import functools
 import itertools 
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--pdbs', nargs="*", type=str, default="3CLN")
 parser.add_argument('--selections', nargs="*", type=str, default="backbone and segid A")
 parser.add_argument('--get_cartesian', type=bool, default=True, help="MDA data extraction")
 
-def load_mmtf(pdbs: List[str]):
-    us = list(map(lambda pdb: mda.fetch_mmtf(pdb), pdbs)) #List of universes
-    return us
-  
-def get_atomgroups(mda_universes: List[mda.Universe], selections: List[str] = "backbone and segid A"):
-    if len(selections) == 1: 
-        print("there is one atom selection criteria...; Applying the same selection for all molecules!")
-        selections = selections * len(mda_universes) #proliferate selection of elements same as pdb lists 
-    else: 
-        assert len(selections) == len(mda_universes), "number of Universes and selections should match!"
+class PersistentHomology(object):
+    def __init__(self, args: argpase.ArgumentParser):
+        pdbs = args.pdbs
+        selections = args.selections
+        get_cartesian = args.get_cartesian
         
-    ags = list(map(lambda u, sel: u.select_atoms(sel), mda_universes, selections ))
-    return ags
+        self.pdbs = pdbs
+        self.selections = selections
+        self.get_cartesian = get_cartesian
 
-def birth_and_death(mda_universes_or_atomgroups: Union[List[mda.Universe], List[mda.AtomGroup]], get_cartesian: bool = True, selections: List[str] = "backbone and segid A"):
-    if isinstance(mda_universes_or_atomgroups[0], mda.Universe):
-        ags = get_atomgroups(mda_universes_or_atomgroups, selections)
-    else:
-        ags = mda_universes_or_atomgroups
-        
-    if get_cartesian:
-        information = list(map(lambda ag: ag.atoms.positions, ags )) #List of atomgroup positions
-    else:
-        raise NotImplementedError("Not implemented for non-positional information!")
+    @staticmethod
+    def load_mmtf(self, pdbs: List[str]):
+        us = list(map(lambda pdb: mda.fetch_mmtf(pdb), pdbs)) #List of universes
+        return us
     
-    Rs = list(map(lambda info: ripser.ripser(info)["dgms"][1], information ))
-    return Rs
+    @staticmethod
+    def get_atomgroups(self, mda_universes: List[mda.Universe], selections: List[str] = "backbone and segid A"):
+        if len(selections) == 1: 
+            print("there is one atom selection criteria...; Applying the same selection for all molecules!")
+            selections = selections * len(mda_universes) #proliferate selection of elements same as pdb lists 
+        else: 
+            assert len(selections) == len(mda_universes), "number of Universes and selections should match!"
 
-def get_wassersteins(ripser_objects: List[ripser.ripser]):
-    assert len(ripser_objects) >= 2, "for Wasserstein, it must have more than two Ripser objects!"
-    ripser_pair = list(itertools.combinations(ripser_objects, 2))
-    wdists = list(map(lambda pair: persim.wasserstein(*pair), ripser_pair ))
-    return wdists
-  
+        ags = list(map(lambda u, sel: u.select_atoms(sel), mda_universes, selections ))
+        return ags
+    
+    @staticmethod
+    def birth_and_death(self, mda_universes_or_atomgroups: Union[List[mda.Universe], List[mda.AtomGroup]], get_cartesian: bool = True, selections: List[str] = "backbone and segid A"):
+        if isinstance(mda_universes_or_atomgroups[0], mda.Universe):
+            ags = get_atomgroups(mda_universes_or_atomgroups, selections)
+        else:
+            ags = mda_universes_or_atomgroups
+
+        if get_cartesian:
+            information = list(map(lambda ag: ag.atoms.positions, ags )) #List of atomgroup positions
+        else:
+            raise NotImplementedError("Not implemented for non-positional information!")
+
+        Rs = list(map(lambda info: ripser.ripser(info)["dgms"][1], information ))
+        return Rs
+
+    @staticmethod
+    def get_wassersteins(self, ripser_objects: List[ripser.ripser]):
+        assert len(ripser_objects) >= 2, "for Wasserstein, it must have more than two Ripser objects!"
+        ripser_pair = list(itertools.combinations(ripser_objects, 2))
+        wdists = list(map(lambda pair: persim.wasserstein(*pair), ripser_pair ))
+        return wdists
+    
+    @property
+    def calculate_wdists(self, ):
+        us = self.load_mmtf(self.pdbs)
+        ags = self.get_atomgroups(us, self.selections)
+        Rs = self.birth_and_death(ags, self.get_cartesian, self.selections)
+        wdists = self.get_wassersteins(Rs)
+        return us, ags, Rs, wdists
+    
 if __name__ == "__main__":
     args = parser.parse_args()
-    us = load_mmtf(args.pdbs)
-    ags = get_atomgroups(us, args.selections)
-    Rs = birth_and_death(ags, args.get_cartesian, args.selections)
-    wdists = get_wassersteins(Rs)
+#     us = load_mmtf(args.pdbs)
+#     ags = get_atomgroups(us, args.selections)
+#     Rs = birth_and_death(ags, args.get_cartesian, args.selections)
+#     wdists = get_wassersteins(Rs)
+    ph = PersistentHomology(args)
+    us, ags, Rs, wdists = ph.calculate_wdists
     print(Rs, wdists)
 
     
