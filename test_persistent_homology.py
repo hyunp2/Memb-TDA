75eef4e81222fd23b048a3efb397f74a9db5d87b
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-08-15 17:27:42 -0500

    Update test_persistent_homology.py

--- test_persistent_homology.py
+++ test_persistent_homology.py
@@ -25,11 +25,13 @@ class PersistentHomology(object):
 #         self.get_cartesian = get_cartesian
 
     @staticmethod
+    @functools.lru_cache(maxsize=None)
     def load_mmtf(pdbs: List[str]):
         us = list(map(lambda pdb: mda.fetch_mmtf(pdb), pdbs)) #List of universes
         return us
     
     @staticmethod
+    @functools.lru_cache(maxsize=None)
     def get_atomgroups(mda_universes: List[mda.Universe], selections: List[str] = "backbone and segid A"):
         if len(selections) == 1: 
             print("there is one atom selection criteria...; Applying the same selection for all molecules!")
@@ -41,6 +43,7 @@ class PersistentHomology(object):
         return ags
     
     @staticmethod
+    @functools.lru_cache(maxsize=None)
     def birth_and_death(mda_universes_or_atomgroups: Union[List[mda.Universe], List[mda.AtomGroup]], get_cartesian: bool = True, selections: List[str] = "backbone and segid A"):
         if isinstance(mda_universes_or_atomgroups[0], mda.Universe):
             ags = get_atomgroups(mda_universes_or_atomgroups, selections)
@@ -56,6 +59,7 @@ class PersistentHomology(object):
         return Rs
 
     @staticmethod
+    @functools.lru_cache(maxsize=None)
     def get_wassersteins(ripser_objects: List[ripser.ripser]):
         assert len(ripser_objects) >= 2, "for Wasserstein, it must have more than two Ripser objects!"
         ripser_pair = list(itertools.combinations(ripser_objects, 2))
