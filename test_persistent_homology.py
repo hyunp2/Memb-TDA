2935291a2c3e787fe8902516966fb6d732c5013f
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-09-12 16:45:48 -0500

    Update test_persistent_homology.py

--- test_persistent_homology.py
+++ test_persistent_homology.py
@@ -5,10 +5,16 @@ import argparse
 from typing import *
 import functools
 import itertools 
-
+from MDAnalysis.analysis.base import AnalysisFromFunction
+from MDAnalysis.analysis.align import AlignTraj
+from MDAnalysis import transformations
+import functools
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--pdbs', nargs="*", type=str, default="3CLN")
+parser.add_argument('--pdb', type=str, default=None)
+parser.add_argument('--psf', type=str, default=None)
+parser.add_argument('--trajs', nargs="*", type=str, default=None) #List of dcds
 parser.add_argument('--selections', nargs="*", type=str, default="backbone and segid A")
 parser.add_argument('--get_cartesian', type=bool, default=True, help="MDA data extraction")
 
@@ -30,25 +36,56 @@ class PersistentHomology(object):
         return us
     
     @staticmethod
-    def get_atomgroups(mda_universes: List[mda.Universe], selections: List[str] = "backbone and segid A"):
-        if len(selections) == 1: 
-            print("there is one atom selection criteria...; Applying the same selection for all molecules!")
-            selections = selections * len(mda_universes) #proliferate selection of elements same as pdb lists 
-        else: 
-            assert len(selections) == len(mda_universes), "number of Universes and selections should match!"
-
-        ags = list(map(lambda u, sel: u.select_atoms(sel), mda_universes, selections ))
-        return ags
+    def traj_preprocessing(prot_traj, prot_ref, align_selection):
+        box_dim = prot_traj.trajectory.ts.dimensions 
+        transform = transformations.boxdimensions.set_dimensions(box_dim)
+        prot_traj.trajectory.add_transformations(transform)
+        AlignTraj(prot_traj, prot_ref, select=align_selection, in_memory=True).run()
+        return prot_traj
     
     @staticmethod
-    def birth_and_death(mda_universes_or_atomgroups: Union[List[mda.Universe], List[mda.AtomGroup]], get_cartesian: bool = True, selections: List[str] = "backbone and segid A"):
-        if isinstance(mda_universes_or_atomgroups[0], mda.Universe):
-            ags = get_atomgroups(mda_universes_or_atomgroups, selections)
-        else:
-            ags = mda_universes_or_atomgroups
+    def load_traj(pdb: str, psf: str, trajs: List[str], selections: str):
+        assert (pdb is not None) or (psf is not None), "At least either PDB of PSF should be provided..."
+        assert trajs is not None, "DCD(s) must be provided"
+        top = pdb if (pdb is not None) else psf
+        universe = mda.Universe(top, *trajs)
+        reference = mda.Universe(top)
+        print("MDA Universe is created")
 
-        if get_cartesian:
+        prot_traj = PersistentHomology.traj_preprocessing(universe, reference, selections)
+        print("Aligned MDA Universe is RETURNED!")
+
+        return reference, prot_traj #universes
+    
+    @staticmethod
+    def get_atomgroups(mda_universes: List[mda.Universe], selections: List[str] = "backbone and segid A"):
+        if isinstance(mda_universes, list):
+            if len(selections) == 1: 
+                print("there is one atom selection criteria...; Applying the same selection for all molecules!")
+                selections = selections * len(mda_universes) #proliferate selection of elements same as pdb lists 
+            else: 
+                assert len(selections) == len(mda_universes), "number of Universes and selections should match!"
+
+            ags = list(map(lambda u, sel: u.select_atoms(sel), mda_universes, selections ))
+            return ags
+        else:
+            ag = [mda_universes.select_atoms(selections[0])] #Make it into a List of AtomGroup
+            return ag #List[one_AtomGroup]
+    
+    @staticmethod
+    def birth_and_death(mda_universes_or_atomgroups: Union[List[mda.Universe], List[mda.AtomGroup]], get_cartesian: bool = True, selections: List[str] = "backbone and segid A", traj_flag: bool=False):
+        if isinstance(mda_universes_or_atomgroups[0], mda.Universe):
+            ags = PersistentHomology.get_atomgroups(mda_universes_or_atomgroups, selections)
+        else:
+            ags = mda_universes_or_atomgroups #List of AtomGroups 
+
+        if get_cartesian and len(ags) >= 2 and not traj_flag:
             information = list(map(lambda ag: ag.atoms.positions, ags )) #List of atomgroup positions
+        elif get_cartesian and len(ags) == 1 and traj_flag:
+            prot_traj = ags[0].universe #back to universe
+            coords = AnalysisFromFunction(lambda ag: ag.positions.copy(),
+                                   prot_traj.atoms.select_atoms(selections[0])).run().results['timeseries']
+            information = np.split(coords, indices_or_sections=coords.shape[0], axis=0) #[(L,3)] * B
         else:
             raise NotImplementedError("Not implemented for non-positional information!")
 
@@ -56,19 +93,35 @@ class PersistentHomology(object):
         return Rs
 
     @staticmethod
-    def get_wassersteins(ripser_objects: List[ripser.ripser]):
-        assert len(ripser_objects) >= 2, "for Wasserstein, it must have more than two Ripser objects!"
-        ripser_pair = list(itertools.combinations(ripser_objects, 2))
-        wdists = list(map(lambda pair: persim.wasserstein(*pair), ripser_pair ))
-        return wdists
-    
+    def get_wassersteins(ripser_objects: List[ripser.ripser], traj_flag: bool=False):
+        if not traj_flag:
+            assert len(ripser_objects) >= 2, "for Wasserstein, it must have more than two Ripser objects!"
+            ripser_pair = list(itertools.combinations(ripser_objects, 2))
+            wdists = list(map(lambda pair: persim.wasserstein(*pair), ripser_pair ))
+            return wdists
+        else:
+            wdists = list(map(lambda pair: functols.partial(persim.wasserstein, dgm1=ripser_objects[0])(dgm2 = pair), ripser_objects[slice(1, None)] ))
+            return wdists
+            
     @property
-    def calculate_wdists(self, ):
+    def calculate_wdists_pdbs(self, ):
         us = self.load_mmtf(self.pdbs)
         ags = self.get_atomgroups(us, self.selections)
         Rs = self.birth_and_death(ags, self.get_cartesian, self.selections)
         wdists = self.get_wassersteins(Rs)
         return us, ags, Rs, wdists
+    
+    @property
+    def calculate_wdists_trajs(self, ):
+        reference, prot_traj = self.load_traj(self.pdbs)
+        ags_ref = self.get_atomgroups(reference, self.selections)
+        ags_trajs = self.get_atomgroups(prot_traj, self.selections)
+        traj_flag = (self.trajs is not None)
+        Rs_ref = self.birth_and_death(ags_ref, self.get_cartesian, self.selections, traj_flag)
+        Rs_trajs = self.birth_and_death(ags_trajs, self.get_cartesian, self.selections, traj_flag)
+        Rs = Rs_ref + Rs_trajs 
+        wdists = self.get_wassersteins(Rs, traj_flag)
+        return [reference, prot_traj], [ags_ref, ags_trajs], Rs, wdists
     
 if __name__ == "__main__":
     args = parser.parse_args()
