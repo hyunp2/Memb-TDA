e862ed1b34792d91d9ac1e208566c6fc0ee92e8f
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-09-22 13:21:46 -0500

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn import metrics
 from scipy import optimize
 import warnings
+import math
+import torch
 
 def wasserstein(dgm1, dgm2, matching=False):
     """
@@ -32,20 +34,20 @@ def wasserstein(dgm1, dgm2, matching=False):
 
     """
 
-    S = np.array(dgm1)
-    M = min(S.shape[0], S.size)
-    if S.size > 0:
-        S = S[np.isfinite(S[:, 1]), :]
+#     S = np.array(dgm1)
+    M = min(S.shape[0], S.numel())
+    if S.numel() > 0:
+        S = S[torch.isfinite(S[:, 1]), :]
         if S.shape[0] < M:
             warnings.warn(
                 "dgm1 has points with non-finite death times;"+
                 "ignoring those points"
             )
             M = S.shape[0]
-    T = np.array(dgm2)
-    N = min(T.shape[0], T.size)
-    if T.size > 0:
-        T = T[np.isfinite(T[:, 1]), :]
+#     T = np.array(dgm2)
+    N = min(T.shape[0], T.numel())
+    if T.numel() > 0:
+        T = T[torch.isfinite(T[:, 1]), :]
         if T.shape[0] < N:
             warnings.warn(
                 "dgm2 has points with non-finite death times;"+
@@ -54,46 +56,47 @@ def wasserstein(dgm1, dgm2, matching=False):
             N = T.shape[0]
 
     if M == 0:
-        S = np.array([[0, 0]])
+#         S = np.array([[0, 0]])
+        S = dgm1.new_zeros((1,2))
         M = 1
     if N == 0:
-        T = np.array([[0, 0]])
+#         T = np.array([[0, 0]])
+        T = dgm2.new_zeros((1,2))
         N = 1
     # Compute CSM between S and dgm2, including points on diagonal
-    DUL = metrics.pairwise.pairwise_distances(S, T)
+#     DUL = metrics.pairwise.pairwise_distances(S, T)
+    DUL = torch.cdist(S,T)
 
     # Put diagonal elements into the matrix
     # Rotate the diagrams to make it easy to find the straight line
     # distance to the diagonal
-    cp = np.cos(np.pi/4)
-    sp = np.sin(np.pi/4)
-    R = np.array([[cp, -sp], [sp, cp]])
-    S = S[:, 0:2].dot(R)
-    T = T[:, 0:2].dot(R)
-    D = np.zeros((M+N, M+N))
-    np.fill_diagonal(D, 0)
+    cp = np.cos(math.pi/4)
+    sp = np.sin(math.pi/4)
+    R = torch.tensor([[cp, -sp], [sp, cp]])
+    S = S[:, 0:2].mm(R)
+    T = T[:, 0:2].mm(R)
+#     D = np.zeros((M+N, M+N))
+    D = dgm1.new_zeros((M+N, M+N))
+#     np.fill_diagonal(D, 0)
+    D.fill_diagonal(0)
     D[0:M, 0:N] = DUL
-    UR = np.inf*np.ones((M, M))
-    np.fill_diagonal(UR, S[:, 1])
+    UR = torch.tensor(float('inf')) * dgm1.new_ones((M, M))
+#     np.fill_diagonal(UR, S[:, 1])
+    UR.fill_diagonal(S[:,1])
     D[0:M, N:N+M] = UR
-    UL = np.inf*np.ones((N, N))
-    np.fill_diagonal(UL, T[:, 1])
+    UL = torch.tensor(float('inf')) * dgm1.new_ones((N, N))
+#     np.fill_diagonal(UL, T[:, 1])
+    UL.fill_diagonal(T[:,1])
     D[M:N+M, 0:N] = UL
 
     # Step 2: Run the hungarian algorithm
     matchi, matchj = optimize.linear_sum_assignment(D)
-    matchdist = np.sum(D[matchi, matchj])
-
-    if matching:
-        matchidx = [(i, j) for i, j in zip(matchi, matchj)]
-        ret = np.zeros((len(matchidx), 3))
-        ret[:, 0:2] = np.array(matchidx)
-        ret[:, 2] = D[matchi, matchj]
-        # Indicate diagonally matched points
-        ret[ret[:, 0] >= M, 0] = -1
-        ret[ret[:, 1] >= N, 1] = -1
-        # Exclude diagonal to diagonal
-        ret = ret[ret[:, 0] + ret[:, 1] != -2, :] 
-        return matchdist, ret
+    matchdist = D[matchi, matchj].sum()
 
     return matchdist
+
+if __name__ == "__main__":
+    x = torch.randn(100,2)
+    y = torch.randn(30, 2)
+    z = wassersteinx(x, y)
+    print(z)
