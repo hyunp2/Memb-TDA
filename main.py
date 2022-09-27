ca467ce0b17281da64ffb69e0850c01e9ab4b192
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-09-27 10:23:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default="/grand/ACO2RDS/xiaoliyan/hMOF/cif") 
+    parser.add_argument('--save_dir', type=str, default=os.getcwd())  
+    parser.add_argument('--filename', type=str, default="default.pickle")  
+    parser.add_argument('--maxdim', type=int, default=1)  
+    parser.add_argument('--multiprocessing', action="store_true")  
+    parser.add_argument('--tensor', action="store_true", help="DEPRECATED!")  
+    parser.add_argument('--train_frac', type=float, default=0.8)  
+    parser.add_argument('--pin_memory', type=bool, default=True)  
+    parser.add_argument('--num_workers', type=int, default=0)  
+    parser.add_argument('--batch_size', type=int, default=32)  
+    parser.add_argument('--psf', type=str, default=None)  
+    parser.add_argument('--pdb', type=str, default=None)  
+    parser.add_argument('--last', type=int, default=200) 
+    parser.add_argument('--trajs', default=None, nargs="*")  
+    parser.add_argument('--atom_selection', type=str, default="backbone")  
+
+    args = parser.parse_args()
+    return args
