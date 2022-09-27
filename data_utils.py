4df7461b8381f44fa9c1c561bc83198a2e658dcc
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-09-27 10:23:46 -0500

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -41,30 +41,10 @@ from MDAnalysis.analysis.base import AnalysisFromFunction
 from MDAnalysis.analysis.align import AlignTraj
 from MDAnalysis import transformations
 from math_utils import wasserstein
+from main import get_args
 
 warnings.simplefilter("ignore")
 warnings.filterwarnings("ignore")
-
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', type=str, default="/grand/ACO2RDS/xiaoliyan/hMOF/cif") 
-    parser.add_argument('--save_dir', type=str, default=os.getcwd())  
-    parser.add_argument('--filename', type=str, default="default.pickle")  
-    parser.add_argument('--maxdim', type=int, default=1)  
-    parser.add_argument('--multiprocessing', action="store_true")  
-    parser.add_argument('--tensor', action="store_true", help="DEPRECATED!")  
-    parser.add_argument('--train_frac', type=float, default=0.8)  
-    parser.add_argument('--pin_memory', type=bool, default=True)  
-    parser.add_argument('--num_workers', type=int, default=0)  
-    parser.add_argument('--batch_size', type=int, default=32)  
-    parser.add_argument('--psf', type=str, default=None)  
-    parser.add_argument('--pdb', type=str, default=None)  
-    parser.add_argument('--last', type=int, default=200) 
-    parser.add_argument('--trajs', default=None, nargs="*")  
-    parser.add_argument('--atom_selection', type=str, default="backbone")  
-
-    args = parser.parse_args()
-    return args
 
 def _get_split_sizes(train_frac: float, full_dataset: Dataset) -> Tuple[int, int, int]:
     """DONE: Need to change split schemes!"""
