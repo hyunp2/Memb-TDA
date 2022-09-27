7fa7d76ccb96523d1196a69690d2522bc15e4973
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-09-27 13:27:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -84,6 +84,7 @@ def get_args():
     parser.add_argument('--gradient_clip', type=float, default=None) 
     parser.add_argument('--accumulate_grad_batches', type=int, default=1) 
     parser.add_argument('--resume', action='store_true')
+    parser.add_argument('--gpu', action='store_true')
     
     #Model utils
     parser.add_argument('--backbone', type=str, default='physnet', choices=["mpnn"])
