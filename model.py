7acfe610ddf2170d88e206c6d6cc756459bd82e5
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-09-27 13:17:52 -0500

    Update model.py

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ from torch_scatter import scatter_mean, scatter_add, scatter_max, scatter
 from torch_geometric.data import Data
 from torch_geometric.nn import MessagePassing, radius_graph
 
-__all__ == ["MPNN"]
+__all__ = ["MPNN"]
 
 class GaussianSmearing(torch.nn.Module):
     def __init__(self, start=0.0, stop=5.0, num_gaussians=50):
