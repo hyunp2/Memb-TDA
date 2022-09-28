f33149a4bac8f14ea87d6d7c3cc1de248e078d01
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-09-27 21:01:07 -0500

    Update data_utils_mem.py

--- data_utils_mem.py
+++ data_utils_mem.py
@@ -168,11 +168,12 @@ class PH_Featurizer_Dataset(Dataset):
         _temps = np.repeat(sorted(glob.glob(os.path.join(self.save_dir, "TEMP*dat"))), 4).tolist() #4 is 4 patching scheme by Andres
         _coords = sorted(glob.glob(os.path.join(self.save_dir, "coords*pickle")))
         _phs = sorted(glob.glob(os.path.join(self.save_dir, "PH*pickle")))
-        print(_temps)
+#         print(_temps)
         zips = zip(_coords, _phs, _temps) #zip to shortest
 
         self.graph_input_list, self.Rs_total, self.mem_temp_list, self.Rs_list_tensor = list(zip(*[self.get_values(coord_filename, ph_filename, temp_filename) for coord_filename, ph_filename, temp_filename in zips]))
         #ABOVE: self.graph_input_list is a LIST of <list of coordinates>; number of elements is number of pickle files
+        print("PASSED")
         self.graph_input_list, self.Rs_total, self.mem_temp_list, self.Rs_list_tensor = list(map(lambda one_list: functools.reduce(lambda a, b: a+b, one_list ), (self.graph_input_list, self.Rs_total, self.mem_temp_list, self.Rs_list_tensor) ))
         #ABOVE: self.graph_input_list is a <list of coordinates>; number of elements is number of pickle files * num_coords per file
         del self.coords_ref
