63827741534608bc875dd22d477c62db5bd68c15
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-09-27 21:40:15 -0500

    Update data_utils_mem.py

--- data_utils_mem.py
+++ data_utils_mem.py
@@ -174,8 +174,11 @@ class PH_Featurizer_Dataset(Dataset):
 
         self.graph_input_list, self.Rs_total, self.mem_temp_list, self.Rs_list_tensor = list(zip(*[self.get_values(coord_filename, ph_filename, temp_filename) for coord_filename, ph_filename, temp_filename in zips]))
         #ABOVE: self.graph_input_list is a LIST of <list of coordinates>; number of elements is number of pickle files
-        print(self.graph_input_list)
-        self.graph_input_list, self.Rs_total, self.mem_temp_list, self.Rs_list_tensor = list(map(lambda one_list: functools.reduce(lambda a, b: a+b, one_list ), (self.graph_input_list, self.Rs_total, self.mem_temp_list, self.Rs_list_tensor) ))
+#         print(self.graph_input_list)
+        if not self.ignore_topologicallayer:
+            self.graph_input_list, self.Rs_total, self.mem_temp_list, self.Rs_list_tensor = list(map(lambda one_list: functools.reduce(lambda a, b: a+b, one_list ), (self.graph_input_list, self.Rs_total, self.mem_temp_list, self.Rs_list_tensor) ))
+        else:
+            self.graph_input_list, self.Rs_total, self.mem_temp_list = list(map(lambda one_list: functools.reduce(lambda a, b: a+b, one_list ), (self.graph_input_list, self.Rs_total, self.mem_temp_list) ))
         #ABOVE: self.graph_input_list is a <list of coordinates>; number of elements is number of pickle files * num_coords per file
         del self.coords_ref
         del self.coords_traj
