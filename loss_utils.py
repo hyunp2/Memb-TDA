bea9dc51f4f8df76df8dfb8d017c4982d5f5d0ad
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-10-26 16:17:04 -0500

    Update loss_utils.py

--- loss_utils.py
+++ loss_utils.py
@@ -11,7 +11,7 @@ def ce_loss(y_true: Union[torch.LongTensor, torch.FloatTensor], y_pred: torch.Fl
 #     onehots.index_select(dim=0, index = y_true.view(-1,).long() - 283) # -->(Batch, numclass)
     assert y_true.size(0) == y_pred.size(0), "Batch size must match!"
     ranges = torch.arange(0, 48).to(y_pred).long() #48 temp bins
-    y_true = ranges.index_select(dim=0, index = y_true.view(-1,).long() - 283) # --> (Batch, ) of LongTensor;; y_pred is (Batch, numclasses)
+    y_true = ranges.index_select(dim=0, index = y_true.to(y_pred).view(-1,).long() - 283) # --> (Batch, ) of LongTensor;; y_pred is (Batch, numclasses)
     ce = torch.nn.CrossEntropyLoss()
     loss = ce(y_pred, y_true)
     return loss
@@ -25,7 +25,7 @@ def reg_loss(y_true: Union[torch.LongTensor, torch.FloatTensor], y_pred: torch.F
     y_pred_expected_T = y_pred_probs * ranges[None, :]  #-->(Batch, numclass)
     y_pred_expected_T = y_pred_expected_T.sum(dim=-1) #-->(Batch,)
     mse = torch.nn.MSELoss()
-    loss = mse(y_true.view(-1,), y_pred_expected_T.view(-1,) )
+    loss = mse(y_true.to(y_pred).view(-1,), y_pred_expected_T.view(-1,) )
     return loss
   
 def contrastive_loss(y_true: Union[torch.LongTensor, torch.FloatTensor], y_pred_tensor: torch.FloatTensor):
