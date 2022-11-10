07c64c7db4188f71e96b137a41f006cd827e59d5
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-11-10 13:38:22 -0600

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -41,5 +41,93 @@ from torch.distributed.fsdp.wrap import (
 
 from train_utils import load_state, single_val, single_test
 
+def validate_and_test(model: nn.Module,
+          get_loss_func: _Loss,
+          val_dataloader: DataLoader,
+	  test_dataloader: DataLoader,
+          logger: Logger,
+          args):
+    """Includes evaluation and testing as well!"""
+
+    #DDP options
+    #Init distributed MUST be called in run() function, which calls this train function!
+
+    device = torch.cuda.current_device()
+    model.to(device=device)
+    local_rank = get_local_rank()
+    world_size = dist.get_world_size() if dist.is_initialized() else 1
+    tmetrics = torchmetrics.MeanAbsoluteError()
+    
+    #DDP Model
+    if dist.is_initialized() and not args.shard:
+        model = DistributedDataParallel(model, device_ids=[local_rank], output_device=local_rank)
+        model._set_static_graph()
+        print(f"DDP is enabled {dist.is_initialized()} and this is local rank {local_rank} and sharding is {args.shard}!!")    
+        model.train()
+        if args.log: logger.start_watching(model) #watch a model!
+    elif dist.is_initialized() and args.shard:
+        my_auto_wrap_policy = functools.partial(
+            default_auto_wrap_policy, min_num_params=100
+        )
+        torch.cuda.set_device(local_rank)
+        model = FSDP(model, fsdp_auto_wrap_policy=my_auto_wrap_policy)
+
+    init_start_event = torch.cuda.Event(enable_timing=True)
+    init_end_event = torch.cuda.Event(enable_timing=True)
+
+    path_and_name = os.path.join(args.load_ckpt_path, "{}.pth".format(args.name))
+    assert args.resume, "Validation and test must be under resumed keyword..."
+    epoch_start, best_loss = load_state(model, None, None, path_and_name, use_artifacts=args.use_artifacts, logger=logger, name=args.name, model_only=True) 
+    
+    #DDP training: Total stats (But still across multi GPUs)
+    init_start_event.record()
+    
+    model.eval()    	
+
+    ###EVALUATION
+    evaluate = single_val
+    val_loss, loss_metrics, val_predictions = evaluate(args, model, val_dataloader, get_loss_func, optimizer, scheduler, logger, tmetrics, return_data=True) #change to single_val with DDP
+    if dist.is_initialized():
+        val_loss = torch.tensor(val_loss, dtype=torch.float, device=device)
+        loss_metrics = torch.tensor(loss_metrics, dtype=torch.float, device=device)
+        torch.distributed.all_reduce(val_loss, op=torch.distributed.ReduceOp.SUM)
+        val_loss = (val_loss / world_size).item()
+        torch.distributed.all_reduce(loss_metrics, op=torch.distributed.ReduceOp.SUM) #Sum to loss
+        loss_metrics = (loss_metrics / world_size).item()
+    if args.log: 
+        logger.log_metrics({'ALL_REDUCED_val_loss': val_loss}, epoch_idx)
+        logger.log_metrics({'ALL_REDUCED_val_MAE': loss_metrics}, epoch_idx) #zero rank only
+    #zero rank only
+    #mae_reduced = tmetrics.compute() #Synced and reduced autometically!
+    #logger.log_metrics({'ALL_REDUCED_val_mae_loss': mae_reduced.item()}, epoch_idx) #zero rank only
+    tmetrics.reset()
+    #scheduler_re.step(val_loss) #Not on individual stats but the total stats
+
+    ###TESTING
+    test_loss, loss_metrics, test_predictions = single_test(args, model, test_dataloader, get_loss_func, optimizer, scheduler, logger, tmetrics, return_data=True) #change to single_val with DDP
+    if dist.is_initialized():
+        test_loss = torch.tensor(test_loss, dtype=torch.float, device=device)
+        loss_metrics = torch.tensor(loss_metrics, dtype=torch.float, device=device)
+        torch.distributed.all_reduce(test_loss, op=torch.distributed.ReduceOp.SUM)
+        test_loss = (test_loss / world_size).item()
+        torch.distributed.all_reduce(loss_metrics, op=torch.distributed.ReduceOp.SUM) #Sum to loss
+        loss_metrics = (loss_metrics / world_size).item()
+    if args.log: 
+        logger.log_metrics({'ALL_REDUCED_test_loss': test_loss}, epoch_idx) #zero rank only
+    logger.log_metrics({'ALL_REDUCED_test_MAE': loss_metrics}, epoch_idx) #zero rank only
+    #mae_reduced = tmetrics.compute() #Synced and reduced autometically!
+    #logger.log_metrics({'ALL_REDUCED_test_mae_loss': mae_reduced.item()}, epoch_idx) #zero rank only
+    tmetrics.reset()
+
+    init_end_event.record()
+
+    if local_rank == 0:
+        print(f"CUDA event elapsed time: {init_start_event.elapsed_time(init_end_event) / 1000}sec")
+	
+    print(cf.on_green("Saving returned validation and test_predictions!"))
+    np.savez("PH_val_test.npz", validation=val_predictions.detach().cpu().numpy(), test=test_predictions.detach().cpu().numpy())
+    
+    print(cf.on_yellow("Validation and test are OVER..."))
+    dist.destroy_process_group()
 
 
