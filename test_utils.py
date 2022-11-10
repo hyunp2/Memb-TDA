75a3e0d1010243420821ec274cb17d20112f78e1
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-11-10 12:51:44 -0600

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,1 +1,44 @@
 
+
+import logging
+import pathlib
+from typing import List, Union
+import os, sys
+import pathlib
+import numpy as np
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import importlib
+try:
+    importlib.import_module("apex.optimizers")
+    from apex.optimizers import FusedAdam, FusedLAMB
+except Exception as e:
+    pass
+from torch.nn.modules.loss import _Loss
+from torch.nn.parallel import DistributedDataParallel
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader, DistributedSampler
+from tqdm import tqdm
+from transformers import get_linear_schedule_with_warmup
+from dist_utils import to_cuda, get_local_rank, init_distributed, seed_everything, \
+    using_tensor_cores, increase_l2_fetch_granularity, Logger
+# from transformers import AdamW
+import curtsies.fmtfuncs as cf
+import torchmetrics
+
+# https://fairscale.readthedocs.io/en/latest/
+from fairscale.nn.checkpoint.checkpoint_activations import checkpoint_wrapper #anytime
+from fairscale.experimental.nn.offload import OffloadModel #Single-GPU
+from fairscale.optim.adascale import AdaScale #DDP
+from fairscale.nn.data_parallel import ShardedDataParallel as ShardedDDP #Sharding
+from fairscale.optim.oss import OSS #Sharding
+import shutil
+from torch.distributed.fsdp import FullyShardedDataParallel, CPUOffload
+from torch.distributed.fsdp.wrap import (
+					default_auto_wrap_policy,
+					enable_wrap,
+					wrap,
+					)
+
+
