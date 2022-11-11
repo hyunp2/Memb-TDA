496bfa4664b6a903c3b9ff1ee8473b02e5a5abba
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-11-11 16:08:03 -0600

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -43,7 +43,7 @@ from MDAnalysis.analysis.base import AnalysisFromFunction
 from MDAnalysis.analysis.align import AlignTraj
 from MDAnalysis import transformations
 from math_utils import wasserstein
-from main import get_args
+# from main import get_args
 import mdtraj
 
 __all__ = ["PH_Featurizer_Dataset", "PH_Featurizer_DataLoader"]
