3c2e5a59f769b7cd329592887796551f67805da5
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-11-23 22:45:44 -0600

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -136,7 +136,7 @@ class InferenceDataset(PH_Featurizer_Dataset):
         predictions_all = []
         with torch.inference_mode():
             for batch in dataloader:
-                batch = batch.to(self.device)
+                batch = batch["PH"].to(self.device)
                 predictions = self.model(batch)
                 predictions_all.append(predictions)
         predictions_all = torch.cat(predictions_all, dim=0) #(how_many_patches, 48)
