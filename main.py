49d9ba95991c13439f7710c64a96c5003156b949
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-01-09 14:51:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def get_args():
     parser.add_argument('--data_dir', type=str, default="/Scr/hyunpark/Monster/vaegan_md_gitlab/data") 
     parser.add_argument('--save_dir', type=str, default="/Scr/hyunpark/Protein-TDA/pickled/")  
     parser.add_argument('--load_ckpt_path', type=str, default="/Scr/hyunpark/Protein-TDA/saved")
-    parser.add_argument('--filename', type=str, default="default.pickle")  
+    parser.add_argument('--filename', type=str, default="dppc.pickle")  
     parser.add_argument('--pdb_database', type=str, default="/Scr/arango/Sobolev-Hyun/2-MembTempredict/testing/") 
     parser.add_argument('--search_temp', default=123, help="e.g. keyword for T.123 directory") 
 
