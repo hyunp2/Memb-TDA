3ad12708d3d3e1ce09b6cec946b66988341a6d9b
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-01-19 17:01:30 -0600

    Create download.py

---
+++ download.py
@@ -0,0 +1,176 @@
+import requests
+import os
+import zipfile
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+import tempfile
+import torch
+import sys
+import argparse
+import pandas as pd
+
+all_models = {
+    "jv_formation_energy_peratom_alignn": [
+        "https://figshare.com/ndownloader/files/31458679",
+        1,
+    ],
+    "jv_optb88vdw_total_energy_alignn": [
+        "https://figshare.com/ndownloader/files/31459642",
+        1,
+    ],
+    "jv_optb88vdw_bandgap_alignn": [
+        "https://figshare.com/ndownloader/files/31459636",
+        1,
+    ],
+    "jv_mbj_bandgap_alignn": [
+        "https://figshare.com/ndownloader/files/31458694",
+        1,
+    ],
+    "jv_spillage_alignn": [
+        "https://figshare.com/ndownloader/files/31458736",
+        1,
+    ],
+    "jv_slme_alignn": ["https://figshare.com/ndownloader/files/31458727", 1],
+    "jv_bulk_modulus_kv_alignn": [
+        "https://figshare.com/ndownloader/files/31458649",
+        1,
+    ],
+    "jv_shear_modulus_gv_alignn": [
+        "https://figshare.com/ndownloader/files/31458724",
+        1,
+    ],
+    "jv_n-Seebeck_alignn": [
+        "https://figshare.com/ndownloader/files/31458718",
+        1,
+    ],
+    "jv_n-powerfact_alignn": [
+        "https://figshare.com/ndownloader/files/31458712",
+        1,
+    ],
+    "jv_magmom_oszicar_alignn": [
+        "https://figshare.com/ndownloader/files/31458685",
+        1,
+    ],
+    "jv_kpoint_length_unit_alignn": [
+        "https://figshare.com/ndownloader/files/31458682",
+        1,
+    ],
+    "jv_avg_elec_mass_alignn": [
+        "https://figshare.com/ndownloader/files/31458643",
+        1,
+    ],
+    "jv_avg_hole_mass_alignn": [
+        "https://figshare.com/ndownloader/files/31458646",
+        1,
+    ],
+    "jv_epsx_alignn": ["https://figshare.com/ndownloader/files/31458667", 1],
+    "jv_mepsx_alignn": ["https://figshare.com/ndownloader/files/31458703", 1],
+    "jv_max_efg_alignn": [
+        "https://figshare.com/ndownloader/files/31458691",
+        1,
+    ],
+    "jv_ehull_alignn": ["https://figshare.com/ndownloader/files/31458658", 1],
+    "jv_dfpt_piezo_max_dielectric_alignn": [
+        "https://figshare.com/ndownloader/files/31458652",
+        1,
+    ],
+    "jv_dfpt_piezo_max_dij_alignn": [
+        "https://figshare.com/ndownloader/files/31458655",
+        1,
+    ],
+    "jv_exfoliation_energy_alignn": [
+        "https://figshare.com/ndownloader/files/31458676",
+        1,
+    ],
+    "mp_e_form_alignnn": [
+        "https://figshare.com/ndownloader/files/31458811",
+        1,
+    ],
+    "mp_gappbe_alignnn": [
+        "https://figshare.com/ndownloader/files/31458814",
+        1,
+    ],
+    "qm9_U0_alignn": ["https://figshare.com/ndownloader/files/31459054", 1],
+    "qm9_U_alignn": ["https://figshare.com/ndownloader/files/31459051", 1],
+    "qm9_alpha_alignn": ["https://figshare.com/ndownloader/files/31459027", 1],
+    "qm9_gap_alignn": ["https://figshare.com/ndownloader/files/31459036", 1],
+    "qm9_G_alignn": ["https://figshare.com/ndownloader/files/31459033", 1],
+    "qm9_HOMO_alignn": ["https://figshare.com/ndownloader/files/31459042", 1],
+    "qm9_LUMO_alignn": ["https://figshare.com/ndownloader/files/31459045", 1],
+    "qm9_ZPVE_alignn": ["https://figshare.com/ndownloader/files/31459057", 1],
+    "hmof_co2_absp_alignnn": [
+        "https://figshare.com/ndownloader/files/31459198",
+        5,
+    ],
+    "hmof_max_co2_adsp_alignnn": [
+        "https://figshare.com/ndownloader/files/31459207",
+        1,
+    ],
+    "hmof_surface_area_m2g_alignnn": [
+        "https://figshare.com/ndownloader/files/31459222",
+        1,
+    ],
+    "hmof_surface_area_m2cm3_alignnn": [
+        "https://figshare.com/ndownloader/files/31459219",
+        1,
+    ],
+    "hmof_pld_alignnn": ["https://figshare.com/ndownloader/files/31459216", 1],
+    "hmof_lcd_alignnn": ["https://figshare.com/ndownloader/files/31459201", 1],
+    "hmof_void_fraction_alignnn": [
+        "https://figshare.com/ndownloader/files/31459228",
+        1,
+    ],
+}
+
+
+def get_all_models():
+    """Return the figshare links for models."""
+    return all_models
+
+
+def get_figshare_model(model_name="jv_formation_energy_peratom_alignn"):
+    """Get ALIGNN torch models from figshare."""
+    # https://figshare.com/projects/ALIGNN_models/126478
+
+    tmp = all_models[model_name]
+    url = tmp[0]
+    output_features = tmp[1]
+    zfile = model_name + ".zip"
+    path = str(os.path.join(os.path.dirname(__file__), zfile))
+    if not os.path.isfile(path):
+        response = requests.get(url, stream=True)
+        total_size_in_bytes = int(response.headers.get("content-length", 0))
+        block_size = 1024  # 1 Kibibyte
+        progress_bar = tqdm(
+            total=total_size_in_bytes, unit="iB", unit_scale=True
+        )
+        with open(path, "wb") as file:
+            for data in response.iter_content(block_size):
+                progress_bar.update(len(data))
+                file.write(data)
+        progress_bar.close()
+    zp = zipfile.ZipFile(path)
+    names = zp.namelist()
+    chks = []
+    for i in names:
+        if "checkpoint_" in i and "pt" in i:
+            tmp = i
+            chks.append(i)
+    print("Using chk file", tmp, "from ", chks)
+    print("Path", os.path.abspath(path))
+    # print("Loading the zipfile...", zipfile.ZipFile(path).namelist())
+    data = zipfile.ZipFile(path).read(tmp)
+    model = ALIGNN(
+        ALIGNNConfig(name="alignn", output_features=output_features)
+    )
+    new_file, filename = tempfile.mkstemp()
+    with open(filename, "wb") as f:
+        f.write(data)
+    model.load_state_dict(torch.load(filename, map_location=device)["model"])
+    model.to(device)
+    model.eval()
+    if os.path.exists(filename):
+        os.remove(filename)
+    return model
+
+
