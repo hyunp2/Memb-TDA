49f990c68920abaa113ceb6f0789a6d8b90185ee
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-02-03 18:52:33 -0600

    Create visual_utils.py

---
+++ visual_utils.py
@@ -0,0 +1,190 @@
+#%matplotlib inline
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+from skimage.filters import threshold_otsu
+from scipy.ndimage.morphology import distance_transform_edt
+import ase
+from ase.io import cube
+from ase.io import cif
+import h5py
+import pickle
+import io
+import json
+import re
+# from IPython.display import display, HTML
+from scipy.spatial import Delaunay
+import plotly
+from plotly.graph_objs import graph_objs as go
+# import ipywidgets as widgets
+# plotly.offline.init_notebook_mode(connected=True)
+from plotly.offline import iplot
+import gudhi, gudhi.hera, gudhi.wasserstein, persim
+import mdtraj
+
+def genAlphaSlider(dat,initial=1,step=1,maximum=10,titlePrefix=""): #assume 3D for now
+    ac = gudhi.AlphaComplex(dat)
+    st = ac.create_simplex_tree()
+    skel=list(st.get_skeleton(2))
+    skel.sort(key=lambda s: s[1])
+    points = np.array([ac.get_point(i) for i in range(st.num_vertices())])
+    #lims=[[np.floor(np.min(dat[:,i])),np.ceil(np.max(dat[:,i]))] for i in range(3)]
+    alpha = widgets.FloatSlider(
+        value = initial,
+        min = 0.0,
+        max = maximum,
+        step = step,
+        description = 'Alpha:',
+        readout_format = '.4f'
+    )
+
+
+
+    b1s=np.array([s[0] for s in skel if len(s[0]) == 2 and s[1] <= alpha.value])
+    triangles = np.array([s[0] for s in skel if len(s[0]) == 3 and s[1] <= alpha.value])
+
+
+    pts=go.Scatter3d(
+        x = points[:, 0],
+        y = points[:, 1],
+        z = points[:, 2],
+        mode='markers',
+        marker=dict(
+            size=2,
+            color="cornflowerblue",                # set color to an array/list of desired values
+            #colorscale='Viridis',   # choose a colorscale
+            opacity=.9
+        ),
+        name='H0'
+
+    )
+
+    sfig=[pts]
+
+    linepts={0:[],1:[],2:[]}
+    for i in b1s:
+        linepts[0].append(points[i[0],0])
+        linepts[1].append(points[i[0],1])
+        linepts[2].append(points[i[0],2])
+        linepts[0].append(points[i[1],0])
+        linepts[1].append(points[i[1],1])
+        linepts[2].append(points[i[1],2])
+
+        linepts[0].append(None)
+        linepts[1].append(None)
+        linepts[2].append(None)
+
+    if len(linepts[0])>0:
+        lins=go.Scatter3d(
+            x=linepts[0],
+            y=linepts[1],
+            z=linepts[2],
+            mode='lines',
+            name='H1',
+            marker=dict(
+                size=3,
+                color="#d55e00",                # set color to an array/list of desired values
+                #colorscale='Viridis',   # choose a colorscale
+                opacity=.9
+            )
+        )
+        sfig.append(lins)
+        if len(triangles)>0:
+            mesh = go.Mesh3d(
+                x = points[:, 0],
+                y = points[:, 1],
+                z = points[:, 2],
+                i = triangles[:, 0],
+                j = triangles[:, 1],
+                k = triangles[:, 2],
+                color="#009e73",
+                opacity=.75,
+                name='H2'
+            )
+
+
+            sfig.append(mesh)
+    fig=go.Figure(sfig)
+    fig.update_layout(width=800,height=800)
+    #fig.show()
+
+
+
+
+    def view_SC(alpha):
+        if alpha==0:
+            fig=go.Figure(sfig[0])
+            fig.show()
+        else:
+            b1s=np.array([s[0] for s in skel if len(s[0]) == 2 and s[1] <= alpha])
+
+            linepts={0:[],1:[],2:[]}
+            for i in b1s:
+                linepts[0].append(points[i[0],0])
+                linepts[1].append(points[i[0],1])
+                linepts[2].append(points[i[0],2])
+                linepts[0].append(points[i[1],0])
+                linepts[1].append(points[i[1],1])
+                linepts[2].append(points[i[1],2])
+
+                linepts[0].append(None)
+                linepts[1].append(None)
+                linepts[2].append(None)
+
+            if len(linepts[0])>0:
+                lins=go.Scatter3d(
+                    x=linepts[0],
+                    y=linepts[1],
+                    z=linepts[2],
+                    mode='lines',
+                    name='H1',
+                    marker=dict(
+                        size=3,
+                        color="#d55e00",                # set color to an array/list of desired values
+                        #colorscale='Viridis',   # choose a colorscale
+                        opacity=.85
+                    )
+                )
+                if len(sfig)>1:
+                    sfig[1]=lins
+                else:
+                    sfig.append(lins)
+                triangles = np.array([s[0] for s in skel if len(s[0]) == 3 and s[1] <= alpha])
+                if len(triangles)>0:
+                    mesh = go.Mesh3d(
+                        x = points[:, 0],
+                        y = points[:, 1],
+                        z = points[:, 2],
+                        i = triangles[:, 0],
+                        j = triangles[:, 1],
+                        k = triangles[:, 2],
+                        color="#009e73",
+                        opacity=.5,
+                        name='H2'
+                    )
+
+                    if len(sfig)>2:
+                        sfig[2]=mesh
+                    else:
+                        sfig.append(mesh)
+
+
+            fig=go.Figure(data=sfig,layout=go.Layout(width=800,height=800,
+                                                     title=f"{titlePrefix}:\nSimplicial complex with radius <= {round(float(alpha),5)}",
+                                                    ))
+
+            #fig.show()
+            iplot(fig)
+
+
+    widgets.interact(view_SC, alpha = alpha);
+    return st
+  
+if __name__ == "__main__":
+    from main import get_args
+    args = get_args()
+    pdb = args.pdb
+    data = mdtraj.load(pdb, top=pdb)
+    genAlphaSlider(data, initial=1, step=1, maximum=10, titlePrefix="")
