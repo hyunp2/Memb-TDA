a2e1dbcb259292538c39b4577b76cec35a58390d
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-02-04 23:10:31 -0600

    Update model.py

--- model.py
+++ model.py
@@ -331,10 +331,11 @@ class Vision(torch.nn.Module):
                                                                 torch.nn.Linear(64, NUM_CLASSES), )) #48 temperature classes
 
     def forward(self, img_ph: torch.FloatTensor):
+        device = image_ph.device
         img_ph : List[torch.FloatTensor] = img_ph.detach().cpu().unbind(dim=0)
         img_ph : List[np.ndarray] = list(map(lambda inp: inp.numpy(), img_ph ))
         img_inputs: Dict[str, torch.FloatTensor] = self.feature_extractor(img_ph, return_tensors="pt") #range [-1, 1]
-        img_inputs = dict(pixel_values=img_inputs["pixel_values"].to(img_ph))
+        img_inputs = dict(pixel_values=img_inputs["pixel_values"].to(device))
         out_ph = self.pretrained(**img_inputs).pooler_output #batch, dim
         out = self.last_layer_together(out_ph)
         
