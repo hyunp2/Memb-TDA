cefcb178f793ac9a599c89b0b6f0934d864f024b
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-02-14 02:23:07 -0600

    Update visual_utils.py

--- visual_utils.py
+++ visual_utils.py
@@ -35,10 +35,10 @@ def plot_total_temps(filename: str):
     BINS = 100
     
     fig, ax = plt.subplots() 
-    ax.hist(data["pred"], bins=BINS, density=True, alpha=0.3) #npz has pred; pickle has predictions
-    sns.kdeplot(data=data["pred"].reshape(-1, ), ax=ax)
+    ax.hist(data["pred"], bins=BINS, density=True, alpha=0.2) #npz has pred; pickle has predictions
+    sns.kdeplot(data=data["pred"].reshape(-1, ), ax=ax, color='k')
     ax.set_xlim(280, 330)
-    ax.set_ylim(0, 0.1)
+    ax.set_ylim(0, 0.08)
 
     ax.set_title("Sth")
 #     ax.set_ylim(280, 330)
