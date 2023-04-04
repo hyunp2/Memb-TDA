b6a20df1e1edc4eab9c887eb44bf0c7d23509f33
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-04-04 11:55:54 -0500

    Update interpret_utils.py

--- interpret_utils.py
+++ interpret_utils.py
@@ -85,7 +85,7 @@ def xai(args, images: torch.Tensor, gts: torch.LongTensor, model: torch.nn.Modul
         attr_output = attrs.attribute(images, target=gts.view(-1)) #->(B,C,N,N)
          
     fig, ax = plt.subplots(4,4,figsize=(8,8))
-    for idx in range(images.size()):
+    for idx in range(images.size(0)):
         axx.flatten()[idx].imshow(attr_output[idx].permute(1,2,0).detach().cpu().numpy())
     plt.show()
     return attr_output
