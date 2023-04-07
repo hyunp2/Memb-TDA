c5bee6c3770fc842159530c4def2b78658577a22
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-04-06 22:32:14 -0500

    Update interpret_utils.py

--- interpret_utils.py
+++ interpret_utils.py
@@ -118,9 +118,8 @@ def xai(args, images: torch.Tensor, gts: torch.LongTensor, model: torch.nn.Modul
         attribute_method = Layer4Gradcam
         attrs = attribute_method(model)
         attr_output = attrs.attribute(images, target=gts.view(-1)) #->(B,1,N,N)
-        sizes = images.size()
 #         print(sizes, attr_output.size())
-        attr_output = torch.nn.functional.interpolate(attr_output, (sizes[2], sizes[3]) )
+        attr_output = torch.nn.functional.interpolate(attr_output, (Vision.IMAGE_SIZE, Vision.IMAGE_SIZE) )
     elif method == "lime":
         attribute_method = Lime
         attrs = attribute_method(forward_func=forward_func, similarity_func=similarity_func, perturb_func=perturb_func)
