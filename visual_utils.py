5dbc6020c1effb88666715b712b485cf1e4f5d8f
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-07-10 22:43:38 -0500

    Update visual_utils.py

--- visual_utils.py
+++ visual_utils.py
@@ -300,18 +300,40 @@ def plot_one_temp(filename: str):
             PC_TEMP = 310
             ps_bin = np.searchsorted(bins, PS_TEMP) #-> int
             pc_bin = np.searchsorted(bins, PC_TEMP) #-> int
-            
+
+            #Method 1: Stairs
             # ax.stairs(counts[:ps_bin], bins[:ps_bin+1], alpha=0.2, color='b', fill=True) #PS_color
             # ax.stairs(counts[pc_bin:], bins[pc_bin:], alpha=0.2, color='r', fill=True) #PC_color
-            ps_range = bins[:ps_bin+1]
-            ax.plot(0.5 * (ps_range[:-1] + ps_range[1:]), counts[:ps_bin], color='b', linewidth=3)
-            pc_range = bins[pc_bin:]
-            ax.plot(0.5 * (pc_range[:-1] + pc_range[1:]), counts[pc_bin:], color='r', linewidth=3)
+
+            #Method 2: Plot
+            # ps_range = bins[:ps_bin+1]
+            # ax.plot(0.5 * (ps_range[:-1] + ps_range[1:]), counts[:ps_bin], color='b', linewidth=3)
+            # pc_range = bins[pc_bin:]
+            # ax.plot(0.5 * (pc_range[:-1] + pc_range[1:]), counts[pc_bin:], color='r', linewidth=3)
+
+            #Method 3: Gradation
+            def hex_to_RGB(hex_str):
+                """ #FFFFFF -> [255,255,255]"""
+                #Pass 16 to the integer function for change of base
+                return [int(hex_str[i:i+2], 16) for i in range(1,6,2)]
+
+            def get_color_gradient(c1, c2, n):
+                """
+                Given two hex colors, returns a color gradient
+                with n colors.
+                """
+                assert n > 1
+                c1_rgb = np.array(hex_to_RGB(c1))/255
+                c2_rgb = np.array(hex_to_RGB(c2))/255
+                mix_pcts = [x/(n-1) for x in range(n)]
+                rgb_colors = [((1-mix)*c1_rgb + (mix*c2_rgb)) for mix in mix_pcts]
+                return ["#" + "".join([format(int(round(val*255)), "02x") for val in item]) for item in rgb_colors]
+
+            ax.stairs(counts, bins, color=get_color_gradient("#0000FF", "#FF0000", len(counts)), fill=True, alpha=0.2) #PC_color
                     
             YLIM = [0, 0.16]
             YTICKS = np.linspace(0, 0.16, 9).tolist()
     else:
-        
         if "PS" in os.path.basename(filename).split("_")[0]:
             ax.hist(data["predictions"].detach().cpu().numpy(), bins=BINS, density=True, alpha=0.2, color='b') #PS_color
         elif "PC" in os.path.basename(filename).split("_")[0]:
