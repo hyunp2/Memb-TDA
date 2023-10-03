9531e84893918766486feb168376be2390aec5e0
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-10-03 13:13:10 -0500

    Update interpret_utils.py

--- interpret_utils.py
+++ interpret_utils.py
@@ -139,14 +139,14 @@ def xai(args, images: torch.Tensor, gts: torch.LongTensor, model: torch.nn.Modul
         attr_output = LayerAttribution.interpolate(attr_output, (Vision.IMAGE_SIZE, Vision.IMAGE_SIZE))
     elif method == "gradcam":
         attribute_method = Layer4Gradcam
-        attrs = attribute_method(args, model, method)
-        attr_output = attrs.attribute(images, target=gts.view(-1)) #->(B,1,N,N)
+        attrs = attribute_method(args, model)
+        attr_output = attrs.attribute(images, target=gts.view(-1), method) #->(B,1,N,N)
 #         print(sizes, attr_output.size())
         attr_output = torch.nn.functional.interpolate(attr_output, (Vision.IMAGE_SIZE, Vision.IMAGE_SIZE) )
     elif method == "attention":
         attribute_method = Layer4Gradcam
-        attrs = attribute_method(args, model, method)
-        attr_output = attrs.attribute(images, target=gts.view(-1)) #->(B,1,N,N)
+        attrs = attribute_method(args, model)
+        attr_output = attrs.attribute(images, target=gts.view(-1), method) #->(B,1,N,N)
 #         print(sizes, attr_output.size())
         attr_output = torch.nn.functional.interpolate(attr_output, (Vision.IMAGE_SIZE, Vision.IMAGE_SIZE) )
     elif method == "lime":
